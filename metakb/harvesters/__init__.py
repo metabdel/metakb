c5d81d4441c616d896fd77d521e9f3a4f0846799
Alex H. Wagner, PhD <awagner24@wustl.edu>
2019-10-07 14:56:40 -0500

    init harvester subpackage

---
+++ metakb/harvesters/__init__.py
@@ -0,0 +1,1 @@
+"""A package for loading from VICC resources."""
---
+++ metakb/harvesters/harvester.py
@@ -0,0 +1,8 @@
+"""Base module for all harvesters."""
+from abc import ABCMeta
+
+
+class Harvester(metaclass=ABCMeta):
+    """Abstract base class for all harvester classes."""
+
+    pass
